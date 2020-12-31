b839f07051d4db043628d8df0706a79c77cf266f
Shumpei-Kikuta <7484525976@g.ecc.u-tokyo.ac.jp>
2021-01-01 07:53:44 +0900

    feat: list factors

--- competitive_algorithms/mathematics.py
+++ competitive_algorithms/mathematics.py
@@ -1,6 +1,7 @@
 """数学系のアルゴリズム"""
 import math
-from typing import Tuple
+from typing import Tuple, List, Dict
+from collections import defaultdict
 
 
 def extgcd(a: int, b: int) -> Tuple[int, int, int]:
@@ -30,3 +31,27 @@ def is_prime(n: int) -> bool:
             return False
         i += 1
     return True
+
+
+def list_prime_factor(n: int) -> Dict[int, int]:
+    # TODO: verify
+    """
+    素因数を列挙するアルゴリズムO(√n)
+    nの素因数は高々√nであることを利用(自身が素数でない場合)
+    """
+    factors = defaultdict(int)
+    if n == 1:
+        raise ValueError("1 does not have factors!")
+    if is_prime(n):
+        factors[n] += 1
+        return factors
+    else:
+        i = 2
+        while(i * i <= n):
+            while (n % i == 0):
+                n = n // i
+                factors[i] += 1
+            i += 1
+        if n != 1:
+            factors[n] += 1
+        return factors
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,6 +1,6 @@
 import pytest
 
-from competitive_algorithms.mathematics import extgcd, is_prime
+from competitive_algorithms.mathematics import extgcd, is_prime, list_prime_factor
 
 
 @pytest.mark.parametrize("a,b", {
@@ -19,3 +19,20 @@ def test_extgcd(a, b):
 def test_is_prime(n, result):
     assert is_prime(n) == result
 
+
+@pytest.mark.parametrize("n, result", [
+    pytest.param(18, {2: 1, 3: 2}, id="simple"),
+    pytest.param(21, {3: 1, 7: 1}, id="simple2"),
+    pytest.param(5, {5: 1}, id="prime"),
+    pytest.param(1, {1: 1}, id="corner"),
+    pytest.param(2, {2: 1}, id="corner2"),
+])
+def test_list_prime_factor(n, result):
+    if n == 1:
+        with pytest.raises(ValueError):
+            list_prime_factor(n)
+    else:
+        assert list_prime_factor(n) == result
+
+
+n = int(input())
