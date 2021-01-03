042cc223540b5c1223f29bb9fa210785d334d352
Shumpei-Kikuta <7484525976@g.ecc.u-tokyo.ac.jp>
2021-01-03 10:55:23 +0900

    feat: imos verified

---
+++ problems/abc035/c.py
@@ -0,0 +1,55 @@
+"""
+imos法 verified
+"""
+
+import sys
+from typing import List, Set, Dict, Tuple
+
+sys.setrecursionlimit(10000000)
+# import numpy as np
+from heapq import heapify, heappop, heappush
+import copy
+from math import gcd, ceil
+from collections import deque, defaultdict
+
+# queueとして使う場合 append(右に挿入), popleft(左を取り出す)を使う
+# stackとして使う場合、append, pop(右を取り出す)
+from itertools import accumulate  # 累積和の計算
+
+# A=[1,4,3,4,6,5]
+# print(list(accumulate(A))) #[1, 5, 8, 12, 18, 23]
+from bisect import bisect_left, bisect_right
+
+# bisect_left: 配列の順序関係が崩れない条件で挿入することができる一番左の点
+# bisect_right: 配列の順序関係が崩れない条件で挿入することができる一番右の点
+# A = [1, 1, 1, 2, 2, 2, 4]
+# print(bisect_left(A, 2))  # 3
+# print(bisect_right(A, 2))  # 6
+# print(bisect_left(A, 3))  # 6
+# print(bisect_right(A, 3))  # 6
+from itertools import permutations, combinations  # 順列、組み合わせ
+
+# for i in combinations(A,2):
+#     print(i, end=' ')
+# (1, 2) (1, 3) (1, 4) (2, 3) (2, 4) (3, 4)
+# pow(n,m,p): 繰り返し二乗法での計算
+# from dataclasses import dataclass
+input = sys.stdin.readline
+
+
+def main():
+    N, Q = map(int, input().split())
+    lists = [0] * N
+    for i in range(Q):
+        l, r = map(int, input().split())
+        lists[l - 1] += 1
+        if r < N:
+            lists[r] -= 1
+    sums = list(accumulate(lists))
+    for i in range(N):
+        print(sums[i] % 2, end="")
+    print()
+
+
+if __name__ == "__main__":
+    main()
