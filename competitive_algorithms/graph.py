c239a363e405aaede9d72cc1faf1fdb8bd3bdf41
Shumpei-Kikuta <7484525976@g.ecc.u-tokyo.ac.jp>
2021-01-07 12:27:43 +0900

    feat: dijkstra

--- competitive_algorithms/graph.py
+++ competitive_algorithms/graph.py
@@ -1,5 +1,5 @@
 from typing import Dict, List, Tuple
-from collections import defaultdict
+import heapq
 
 INF = 10 ** 10
 
@@ -12,14 +12,18 @@ class Edge:
 
 
 class Graph:
-    def __init__(self, adjacent_lists: Dict[int, List[Tuple[int, int]]]):
-        self.adjacent_lists = defaultdict(int, adjacent_lists)
+    def __init__(self, adjacent_lists: Dict[int, Dict[int, int]]):
+        # 隣接リスト
+        # {1: {2: 4, 3: 5}}など
+        self.adjacent_lists = adjacent_lists
         self.edges: List[Edge] = self.initialize_edgelists()
+        # TODO: edge_queueの定義
+        self.edge_queues = []
 
     def initialize_edgelists(self):
         edges = []
         for from_ in self.adjacent_lists.keys():
-            for to_, d in self.adjacent_lists[from_]:
+            for to_, d in self.adjacent_lists[from_].items():
                 edge = Edge(from_, to_, d)
                 edges.append(edge)
         return edges
@@ -46,3 +50,61 @@ class Graph:
         else:
             return "NEGATIVE CYCLE"
         return distances
+
+    # def dijkstra(self, s):
+    #     alreadys = [False] * len(self.adjacent_lists)
+    #     distances = {node: INF for node in self.adjacent_lists.keys()}
+    #     now = s
+    #     distances[s] = 0
+    #     for (to_, d) in self.adjacent_lists[now].items():
+    #         heapq.heappush(self.edge_queues, (d, now, to_))
+    #         distances[to_] = d
+    #     alreadys[s] = True
+
+    #     while(len(self.edge_queues) != 0):
+    #         d, _, now = heapq.heappop(self.edge_queues)
+    #         if alreadys[now]:
+    #             continue
+    #         alreadys[now] = True
+    #         for next_node, d in self.adjacent_lists[now].items():
+    #             # if alreadys[next_node]:
+    #             #     continue
+    #             distances[next_node] = min(distances[next_node], distances[now] + self.adjacent_lists[now][next_node])
+    #             heapq.heappush(self.edge_queues, (d, now, next_node))
+    #     return distances
+    def dijkstra(self, s):
+        alreadys = [False] * len(self.adjacent_lists)
+        distances = {node: INF for node in self.adjacent_lists.keys()}
+        now = s
+        distances[s] = 0
+        for (to_, d) in self.adjacent_lists[now].items():
+            heapq.heappush(self.edge_queues, (d, to_))
+            distances[to_] = d
+        alreadys[s] = True
+
+        while len(self.edge_queues) != 0:
+            d, now = heapq.heappop(self.edge_queues)
+            if alreadys[now]:
+                continue
+            alreadys[now] = True
+            for next_node, d in self.adjacent_lists[now].items():
+                if alreadys[next_node]:
+                    continue
+                if distances[next_node] > distances[now] + d:
+                    distances[next_node] = distances[now] + d
+                    heapq.heappush(self.edge_queues, (distances[next_node], next_node))
+        return distances
+
+
+# v, e, r = map(int, input().split())
+# adjacent_lists = {node: dict() for node in range(v)}
+# for _ in range(e):
+#     s, t, d = map(int, input().split())
+#     adjacent_lists[s][t] = d
+# graph = Graph(adjacent_lists)
+# result = graph.dijkstra(r)
+# for v in result.values():
+#     if v == 10**10:
+#         print("INF")
+#     else:
+#         print(v)
